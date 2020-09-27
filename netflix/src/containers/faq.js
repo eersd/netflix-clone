c53e23c79eb7ebe78091b7a44be8c9d8e07c0208
eersada <internets.scholar@gmail.com>
2020-09-27 13:23:37 +0200

    added faq component

---
+++ netflix/src/containers/faq.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Accordion from '../components/accordion';
+import OptForm from '../components/opt-form';
+import faqsData from '../fixtures/faq.json';
+
+export function FaqsContainer() {
+  return (
+    <Accordion>
+      <Accordion.Title>Frequently Asked Questions</Accordion.Title>
+      {faqsData.map((item) => (
+        <Accordion.Item key={item.id}>
+          <Accordion.Header>{item.header}</Accordion.Header>
+          <Accordion.Body>{item.body}</Accordion.Body>
+        </Accordion.Item>
+      ))}
+      <OptForm>
+        <OptForm.Text>Ready to watch? Enter your email to create or restart your membership</OptForm.Text>
+        <OptForm.Input placeholder="Email address" />
+
+        <OptForm.Button>Try 30 Days Free</OptForm.Button>
+      </OptForm>
+    </Accordion>
+  );
+}
